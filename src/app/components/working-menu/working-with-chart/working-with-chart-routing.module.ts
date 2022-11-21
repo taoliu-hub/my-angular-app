928e695ebcb3e8eaca651f18137f89a53ce2c372
Liu Tao <tao.liu1@ncsi.com.cn>
2022-11-21 18:23:36 +0800

    updated for charts

---
+++ src/app/components/working-menu/working-with-chart/working-with-chart-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/auth.guard';
+import { AllChartsComponent } from './all-charts/all-charts.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { DynamicChartComponent } from './dynamic-chart/dynamic-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { PolarAreaChartComponent } from './polar-area-chart/polar-area-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { ScatterChartComponent } from './scatter-chart/scatter-chart.component';
+
+const routes: Routes = [
+  { path: '', canActivate: [AuthGuard], component: AllChartsComponent },
+  { path: 'bar-chart', canActivate: [AuthGuard], component: BarChartComponent },
+  { path: 'bubble-chart', canActivate: [AuthGuard], component: BubbleChartComponent },
+  { path: 'doughnut-chart', canActivate: [AuthGuard], component: DoughnutChartComponent },
+  { path: 'dynamic-chart', canActivate: [AuthGuard], component: DynamicChartComponent },
+  { path: 'line-chart', canActivate: [AuthGuard], component: LineChartComponent },
+  { path: 'pie-chart', canActivate: [AuthGuard], component: PieChartComponent },
+  { path: 'polarArea-chart', canActivate: [AuthGuard], component: PolarAreaChartComponent },
+  { path: 'radar-chart', canActivate: [AuthGuard], component: RadarChartComponent },
+  { path: 'scatter-chart', canActivate: [AuthGuard], component: ScatterChartComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [
+    RouterModule,
+  ]
+})
+export class WorkingWithChartRoutingModule { }
--- src/app/components/working-menu/working-with-chart/working-with-chart.module.ts
+++ src/app/components/working-menu/working-with-chart/working-with-chart.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from "@angular/core";
-import { BarChartComponent } from "./bar-chart/bar-chart.component";
-import { BubbleChartComponent } from "./bubble-chart/bubble-chart.component";
-import { DoughnutChartComponent } from "./doughnut-chart/doughnut-chart.component";
-import { DynamicChartComponent } from "./dynamic-chart/dynamic-chart.component";
-import { LineChartComponent } from "./line-chart/line-chart.component";
-import { PieChartComponent } from "./pie-chart/pie-chart.component";
-import { PolarAreaChartComponent } from "./polar-area-chart/polar-area-chart.component";
-import { RadarChartComponent } from "./radar-chart/radar-chart.component";
-import { ScatterChartComponent } from "./scatter-chart/scatter-chart.component";
+import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { DynamicChartComponent } from './dynamic-chart/dynamic-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { PolarAreaChartComponent } from './polar-area-chart/polar-area-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { ScatterChartComponent } from './scatter-chart/scatter-chart.component';
+import { WorkingWithChartRoutingModule } from './working-with-chart-routing.module';
+import { AllChartsComponent } from './all-charts/all-charts.component';
+
 
 @NgModule({
   declarations: [
+    AllChartsComponent,
     BarChartComponent,
     BubbleChartComponent,
     DoughnutChartComponent,
@@ -23,9 +27,11 @@ import { ChartsModule } from 'ng2-charts';
     ScatterChartComponent
   ],
   imports: [
+    WorkingWithChartRoutingModule,
     ChartsModule
   ],
   exports: [
+    AllChartsComponent,
     BarChartComponent,
     BubbleChartComponent,
     DoughnutChartComponent,
@@ -34,8 +40,7 @@ import { ChartsModule } from 'ng2-charts';
     PieChartComponent,
     PolarAreaChartComponent,
     RadarChartComponent,
-    ScatterChartComponent,
-    ChartsModule
+    ScatterChartComponent
   ]
 })
 export class WorkingWithChartModule { }
